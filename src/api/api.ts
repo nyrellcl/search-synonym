b3f1e42bec59703a9ab21663063acb59b7b4b2f0
nyrellcl <nyrell.leonor2000@gmail.com>
2023-04-18 10:35:50 -0700

    add api file

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,14 @@
 import axios from "axios";
+
+const BASE_API_URL = 'https://api.datamuse.com'
+
+class WordFacade{
+    async getSynonymData(synonym: string){
+        const res = await axios.get(
+            `${BASE_API_URL}/words?rel_syn=${synonym}`
+        );
+        return res.data
+    }
+}
+
+export default new WordFacade()
