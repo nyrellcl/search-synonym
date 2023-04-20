eb0f2a026fbb0c30420e2ce5ea8e748a935fde48
nyrellcl <nyrell.leonor2000@gmail.com>
2023-04-19 21:41:43 -0700

    add error handling in api fetch

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,32 +1,36 @@
-interface Synonyms{
-    findSynonyms(searchTerm: string): Promise<string[]>
+interface Synonyms {
+  findSynonyms(searchTerm: string): Promise<string[]>;
 }
 
+export class SynonymFinder implements Synonyms {
+  private readonly BASE_API_URL: string;
 
-export class SynonymFinder implements Synonyms{
-    private readonly BASE_API_URL: string
+  constructor() {
+    this.BASE_API_URL = "https://api.datamuse.com/words";
+  }
+  public async findSynonyms(searchTerm: string): Promise<string[]> {
+    const queryParam = "rel_syn";
+    const url = `${this.BASE_API_URL}?${queryParam}=${searchTerm}`;
 
-    constructor(){
-        this.BASE_API_URL = 'https://api.datamuse.com/words'
+    try {
+      const res = await fetch(url);
+      const synonyms = await res.json();
+      return synonyms.map((s: any) => s.word);
+    } catch (error) {
+      console.error("Error while fetching synonyms: ", error);
+      throw error;
     }
-    public async findSynonyms(searchTerm: string): Promise<string[]>{
-        const queryParam = 'rel_syn'
-        const url = `${this.BASE_API_URL}?${queryParam}=${searchTerm}`
-        const res = await fetch(url)
-        const synonyms = await res.json()
-        return synonyms.map((s: any)=> s.word)
-    }
+  }
 }
 
-export class SynonymFinderFacade implements Synonyms{
-    private readonly synonym: Synonyms
-    
-    constructor(synonym: Synonyms){
-        this.synonym = synonym
+export class SynonymFinderFacade implements Synonyms {
+  private readonly synonym: Synonyms;
 
-    }
+  constructor(synonym: Synonyms) {
+    this.synonym = synonym;
+  }
 
-    public async findSynonyms(searchTerm: string): Promise<string[]> {
-        return this.synonym.findSynonyms(searchTerm)
-    }
+  public async findSynonyms(searchTerm: string): Promise<string[]> {
+    return this.synonym.findSynonyms(searchTerm);
+  }
 }
